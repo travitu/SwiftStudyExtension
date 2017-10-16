008dece4dc8a8cf5ca68a5ff25f180b5fc42a9c2
travitu <travitu84@gmail.com>
2017-10-16 23:08:18 +0900

    Initial Commit

---
+++ SwiftStudyExtension/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftStudyExtension
+//
+//  Created by Toshikazu Fukuoka on 2017/10/16.
+//  Copyright © 2017年 travitu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
